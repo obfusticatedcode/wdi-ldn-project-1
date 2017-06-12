e33ef567ea5b6196edbd8faab3530ee65d94699f
Chisomo Lungu <chisomo@gmail.com>
2017-06-12 19:54:34 +0100

    figuring out how to get the local currency based on the exchange rate

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,16 @@
 $(() => {
 
   const $threeWordsLocation = $('#three-words-location');
+  const $country = $('#country');
   const $lat = $('#lat');
   const $lng = $('#lng');
-  let currentCountryER = null;
+
+
   //tesing JS works
   console.log(`JS is working fine`);
 
   //setup the map and infoWindow constiable
-  let map, infoWindow = null;
+  let map, infoWindow, newLat, newLng, localCurrency = null;
 
   if($('#map').length > 0) initMap();
 
@@ -28,17 +30,24 @@ $(() => {
 
     });
 
-    google.maps.event.addListener(map, 'dragend', function() {
-      const newLat = map.getCenter().lat();
-      const newLng = map.getCenter().lng();
-      console.log(newLat, newLng);
-      getThreeWords(newLat, newLng);
-    });
     //infoWindow
     infoWindow = new google.maps.InfoWindow;
 
     //geoCoder
     const geocoder = new google.maps.Geocoder;
+
+    google.maps.event.addListener(map, 'dragend', function() {
+
+      newLat = map.getCenter().lat();
+      newLng = map.getCenter().lng();
+
+      const newPos = { newLat, newLng};
+      console.log(newPos);
+      getThreeWords(newLat, newLng);
+
+
+    });
+
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -53,12 +62,13 @@ $(() => {
         infoWindow.setContent('Your current location.');
         infoWindow.open(map);
         map.setCenter(pos);
-        console.log(pos);
+
         getThreeWords(pos.lat, pos.lng);
-        
+
         //trying geoCoder for reverseGeolocation, getting the Country Name
         geocoder.geocode({'location': pos}, (results) => {
           console.log(results[0].address_components[6].long_name);
+          $country.val(results[0].address_components[6].long_name);
         });
 
       }, function() {
@@ -106,15 +116,14 @@ $(() => {
     });
   }
 
+  //get the current exchange rate and map it to the Country name
   $.ajax({
     url: 'http://www.apilayer.net/api/live?access_key=9349182f47c0d493c3853badcdce3cfc',
     method: 'GET'
 
   })
   .done((response) => {
-    console.log(response.quotes);
-    currentCountryER = response.quotes.USDZAR;
-
+    console.log(response.quotes.USDGBP);
   });
 
 
